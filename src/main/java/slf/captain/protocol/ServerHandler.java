85cbc8c73cfc5a9fb077a456f85d02d61989d7e2
wwg-pc <361788095@qq.com>
2020-03-08 16:49:46 +0800

    init

--- src/main/java/slf/captain/protocol/ServerHandler.java
+++ src/main/java/slf/captain/protocol/ServerHandler.java
@@ -3,10 +3,10 @@ package slf.captain.protocol;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class ServerHandler extends SimpleChannelInboundHandler<FFMessage> {
+public class ServerHandler extends SimpleChannelInboundHandler<CaptainMessage> {
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FFMessage ffMessage) throws Exception {
-        System.out.println("Receive Msg = " + ffMessage.toString());
-        channelHandlerContext.writeAndFlush(ffMessage);
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, CaptainMessage captainMessage) {
+        System.out.println("Receive Msg = " + captainMessage.toString());
+        channelHandlerContext.writeAndFlush(captainMessage);
     }
 }
