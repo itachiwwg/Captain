9f3fd7209b46e4011a4544bacbc30e22ec8eccdb
wwg-pc <361788095@qq.com>
2020-03-08 16:51:57 +0800

    init

--- src/main/java/slf/captain/Main.java
+++ src/main/java/slf/captain/Main.java
@@ -1,9 +1,14 @@
 package slf.captain;
 
+import slf.captain.service.FrontServer;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("this is Start line");
-
+    public static void main(String[] args) throws InterruptedException {
+        FrontServer fs = new FrontServer();
+        fs.start();
     }
 }
